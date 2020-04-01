ff45ca1e3f213e11a9717a4622d593be326ddddd
ThreeHDM <juanbilardi@gmail.com>
2020-04-01 20:12:36 -0300

    learning about pass and continue in while loops

--- attack.py
+++ attack.py
@@ -15,11 +15,10 @@ while playerhp > 0:
 
     print("Enemy strikes for", dmg, "points of damage. Current HP is", playerhp)
 
-    if playerhp == 30:
-        print("You have been hurt. You've been teleported to the nearest inn.")
-        break  # stops the loop // breakes out from the loop
+    if playerhp > 30:
+        # pass  # lets you leave an empty block of code
+        continue  # ignores everything after in the loop
 
-    if playerhp == 0:
-        print("Your have died")
-
+    print("You have low health. You've been teleported to the nearest inn")
+    break
 
